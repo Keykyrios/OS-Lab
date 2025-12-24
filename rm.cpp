0618df250e5e4e4256e98b342c9a6b123ebca2b1
Keykyrios <mitrajitghorui@gmail.com>
2025-12-24 19:51:55 +0530

    Update rm.cpp

--- rm.cpp
+++ rm.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <cstdio> // For remove()
-
+#include <cstdio> 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // Check if filename is provided
     if (argc < 2) {
         cout << "Usage: ./rm <filename>" << endl;
         return 1;
     }
 
-    // remove() deletes the file specified
     if (remove(argv[1]) == 0) {
         cout << "File deleted successfully" << endl;
     } else {
